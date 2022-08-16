e62c5df3c874fbd2e31dfbf12b4796ae177d8b90
Rafał Kondziela <hajtpl@gmail.com>
2022-08-16 20:00:19 +0200

    Add Wallet custom manager

--- api/wallets/models.py
+++ api/wallets/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models import F, OuterRef, Subquery
+from django.db.models.aggregates import Sum
+from django.db.models.functions import Coalesce
 
 from api.core.models import CommonModel
 from api.users.models import User
@@ -6,6 +9,34 @@ from api.users.models import User
 
 class Currency(models.TextChoices):
     PLN = "pln", "Polish złoty"
+
+
+class WalletManager(models.Manager):
+    def get_queryset(self):
+        from api.transactions.models import Transaction
+
+        expenses_subquery = (
+            Transaction.objects.filter(wallet_id=OuterRef("id"), is_expense=True)
+            .values("wallet_id")
+            .annotate(total_expenses=Sum("amount"))
+            .values("total_expenses")
+        )
+        income_subquery = (
+            Transaction.objects.filter(wallet_id=OuterRef("id"), is_expense=False)
+            .values("wallet_id")
+            .annotate(total_income=Sum("amount"))
+            .values("total_income")
+        )
+
+        return (
+            super()
+            .get_queryset()
+            .annotate(
+                expenses=Coalesce(Subquery(expenses_subquery), 0),
+                income=Coalesce(Subquery(income_subquery), 0),
+                balance=F("income") - F("expenses"),
+            )
+        )
 
 
 class Wallet(CommonModel):
@@ -19,6 +50,8 @@ class Wallet(CommonModel):
     currency = models.CharField(
         max_length=15, choices=Currency.choices, default=Currency.PLN
     )
+
+    objects = WalletManager()
 
     class Meta:
         constraints = [
